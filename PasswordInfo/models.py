481951dd52e91ec6b3ddc763664fde5e4b026287
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-14 19:31:08 +0000

    alterled user field to include related_name

--- PasswordInfo/models.py
+++ PasswordInfo/models.py
@@ -9,7 +9,7 @@ class PasswordInfo(models.Model):
     """
     password = models.CharField(max_length=200)
     organization = models.CharField(max_length=25)
-    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="passwordinfos")
     username = models.CharField(max_length=30)
 
     def __str__(self) -> str:
