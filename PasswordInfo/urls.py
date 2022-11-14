a50fe6d0c0381a29c466dab8fb70d5be5bfaa6de
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-14 23:40:43 +0000

    urls.py for passwordinfo app added

---
+++ PasswordInfo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PasswordInfoList,AddPasswordInfo,DeletePasswordInfo,RetrievePasswordInfo,UpdatePasswordInfo
+
+
+urlpatterns = [
+    path("addpassword",AddPasswordInfo.as_view()),
+    path("passwords/",PasswordInfoList.as_view()),
+    path('password/<int:pk>',RetrievePasswordInfo.as_view()),
+    path('password/delete/<int:pk>',DeletePasswordInfo.as_view()),
+    path('password/edit/<int:pk>',UpdatePasswordInfo.as_view())
+]
