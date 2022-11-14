0c9d994b66b01e67ab0969953f4a4aeacb3c5128
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-14 23:40:21 +0000

    added passwordinfoserializer

--- PasswordInfo/serializers.py
+++ PasswordInfo/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import PasswordInfo
 
 class PasswordInfoSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = PasswordInfo
-        fields = ["id","username","password","user","organization",]
+        fields = ["id","username","password","user","organization"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id","username"]
