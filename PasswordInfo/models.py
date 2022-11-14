b5c6f1c3de12bcf6a4723c22601fc518c8768ce3
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-14 18:46:11 +0000

    added password info model

--- PasswordInfo/models.py
+++ PasswordInfo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class PasswordInfo(models.Model):
+    """
+        PasswordInfo model
+    """
+    password = models.CharField(max_length=200)
+    organization = models.CharField(max_length=25)
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    username = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.password
+
+    
