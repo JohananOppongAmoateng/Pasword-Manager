a76d808e5fe7a824c07e050402d151eabdcb6c6a
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-17 23:18:51 +0000

    data files

---
+++ data/passwordinfo_data.py
@@ -0,0 +1,26 @@
+passwordinfo = {
+             "password":"ama and kofi",
+            "organization":"description is very short",
+            'username':'2020-11-28T19:24:58.478641+05:30'
+        }
+passwordinfo1 = {
+            "password":"ama and kofi",
+            "organization":"description is very short",
+            'username':'2020-11-28T19:24:58.478641+05:30'
+        }
+passwordinfo2 = {
+             "password":"ama and kofi",
+            "organization":"description is very short",
+            'username':'2020-11-28T19:24:58.478641+05:30'
+        }
+passwordinfo3 = {
+             "password":"ama and kofi",
+            "organization":"description is very short",
+            'username':'2020-11-28T19:24:58.478641+05:30'
+        }
+passwordinfo4 = {
+            "password":"ama and kofi",
+            "organization":"description is very short",
+            'username':'2020-11-28T19:24:58.478641+05:30'
+        }
+
---
+++ data/user_data.py
@@ -0,0 +1,18 @@
+
+
+user = {"username":'johanan',
+        'email':'ama@gmai.com',
+        'first_name':'johanan',
+        'last_name':'amoateng',
+        'password':'passwordama',
+        'password2':'passwordama'
+        }
+        
+user1 = {"username":'johananoa',
+        'email':'ama@gmanmi.com',
+        'first_name':'jom,hanan',
+        'last_name':'amoatn,eng',
+        'password':'passwordmama',
+        'password2':'passwordmama'
+        }
+
