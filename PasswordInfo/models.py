1ca919cbe201543d94fe12e2f5ef7bec61099f28
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2022-11-17 23:21:38 +0000

    increased username maxlength to 60

--- PasswordInfo/models.py
+++ PasswordInfo/models.py
@@ -11,7 +11,7 @@ class PasswordInfo(models.Model):
     password = encrypt(models.CharField(max_length=200))
     organization = encrypt(models.CharField(max_length=25))
     user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="passwordinfos")
-    username = encrypt(models.CharField(max_length=30))
+    username = encrypt(models.CharField(max_length=60))
 
     def __str__(self):
         return self.password
